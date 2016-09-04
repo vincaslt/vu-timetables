8abad97e57f6d86aa4d0044837ef1586e3128f4d
Vincas Stonys <vincaslt@gmail.com>
2016-09-04 17:38:57 +0300

    Add openshift configuration

--- server/index.js
+++ server/index.js
@@ -183,6 +183,9 @@ server.get('/api/timetable/:dept/:courseId', function (req, res, next) {
   return next();
 });
 
-server.listen(8080, '0.0.0.0', function () {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
   console.log('\n%s listening at %s', server.name, server.url);
 });
